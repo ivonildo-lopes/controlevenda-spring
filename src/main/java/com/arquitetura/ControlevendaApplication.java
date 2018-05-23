a60b26363306c1fe59ec06acdfd95a6c71ec76fc
Ivonildo Rodrigues <ivonildolopes.ti@gmail.com>
2018-05-23 16:20:09 -0300

    projeto criado

---
+++ src/main/java/com/arquitetura/ControlevendaApplication.java
@@ -0,0 +1,12 @@
+package com.arquitetura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlevendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlevendaApplication.class, args);
+	}
+}
